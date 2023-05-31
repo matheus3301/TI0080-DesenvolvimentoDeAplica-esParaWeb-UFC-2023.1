8f29af80ad08facaf56ca9ec3d72e0558a55947a
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2023-05-31 16:43:52 -0300

    add: rendering template

--- src/configuration/app.js
+++ src/configuration/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const cors = require('cors');
 
+const authRouter = require('../routes/auth');
+
 const app = express();
 
 app.use(cors());
 
-nunjucks.configure('views', {
+nunjucks.configure('src/views', {
   autoescape: true,
   express: app,
 });
+
+app.use('/', authRouter);
 
 module.exports = app;
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,5 +1,11 @@
 const authRouter = require('express').Router();
 
-authRouter.get('/login', (req, res) => {});
+authRouter.get('/login', (req, res) => {
+  let content = {
+    title: 'Login | SmartTrivia',
+  };
+
+  res.render('login.njk', content);
+});
 
 module.exports = authRouter;
