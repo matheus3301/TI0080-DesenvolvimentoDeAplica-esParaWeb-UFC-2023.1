47f78ecb9dd68e0727293d01a287cd51cbe9a400
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2023-06-14 13:58:25 -0300

    add: seed creating questions

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -4,6 +4,106 @@ const prisma = new PrismaClient();
 async function main() {
   await createTeachers();
   await createStudents();
+  await createQuestions();
+}
+
+async function createQuestions() {
+  await prisma.question.deleteMany({});
+  await prisma.question.create({
+    data: {
+      statement:
+        'Tendo-se 5 objetos diferentes e 7 caixas numeradas de 1 a 7, o número de formas distintas de se guardar um objeto em cada caixa é',
+      title: 'Matemática - Combinatória',
+      teacherId: (await prisma.teacher.findFirst({})).id,
+      choices: {
+        create: [
+          {
+            value: '2520',
+            isCorrect: true,
+          },
+          {
+            value: '1260',
+            isCorrect: false,
+          },
+        ],
+      },
+    },
+    include: {
+      choices: true,
+    },
+  });
+
+  await prisma.question.create({
+    data: {
+      statement:
+        'Vacúolos são estruturas existentes em diferentes tipos celulares, entretanto, o vacúolo de suco celular é uma estrutura típica da célula vegetal. Observe as funções abaixo e marque aquela que não pode ser atribuída ao vacúolo.',
+      title: 'Biologia - Biologia Celular',
+      teacherId: (await prisma.teacher.findFirst({})).id,
+      choices: {
+        create: [
+          {
+            value: 'Participa do processo de respiração celular.',
+            isCorrect: true,
+          },
+          {
+            value: 'Realiza a digestão de componentes celulares.',
+            isCorrect: false,
+          },
+        ],
+      },
+    },
+    include: {
+      choices: true,
+    },
+  });
+
+  await prisma.question.create({
+    data: {
+      statement:
+        'Estrutura geológica é a classificação da litosfera terrestre conforme as suas diferentes origens e as composições de suas rochas. Por seus processos formativos, as estruturas geológicas com condições mais favoráveis à formação de combustíveis fósseis são:',
+      title: 'Geografia - Geografia Física',
+      teacherId: (await prisma.teacher.findFirst({})).id,
+      choices: {
+        create: [
+          {
+            value: 'As bacias sedimentares',
+            isCorrect: true,
+          },
+          {
+            value: 'As plataformas cristalinas',
+            isCorrect: false,
+          },
+        ],
+      },
+    },
+    include: {
+      choices: true,
+    },
+  });
+
+  await prisma.question.create({
+    data: {
+      statement:
+        'Dentro de um elevador, um objeto de peso 100 N está apoiado sobre uma superfície. O elevador está descendo e freando com aceleração vertical e para cima de 0,1 m/s2. Considere a aceleração da gravidade como 10 m/s2. Durante o tempo de frenagem, a força que sustenta o objeto vale, em newtons:',
+      title: 'Física - Dinâmica',
+      teacherId: (await prisma.teacher.findFirst({})).id,
+      choices: {
+        create: [
+          {
+            value: '101',
+            isCorrect: true,
+          },
+          {
+            value: '110',
+            isCorrect: false,
+          },
+        ],
+      },
+    },
+    include: {
+      choices: true,
+    },
+  });
 }
 
 async function createStudents() {
