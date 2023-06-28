38c828df8453409416e33849473af4aafb0a357a
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2023-06-27 22:36:53 -0300

    add: implementing middlewares on question router

--- api/src/routes/questionRouter.js
+++ api/src/routes/questionRouter.js
@@ -1,10 +1,37 @@
 const router = require('express').Router();
 const questionController = require('../controllers/questionController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const userTypeMiddleware = require('../middlewares/userTypeMiddleware');
 
-router.post('/', questionController.createQuestion);
-router.get('/', questionController.getAllQuestions);
-router.get('/:id', questionController.getQuestionById);
-router.delete('/:id', questionController.deleteQuestionById);
-router.put('/:id', questionController.updateQuestionById);
+router.post(
+  '/',
+  authMiddleware,
+  userTypeMiddleware(['TEACHER']),
+  questionController.createQuestion
+);
+router.get(
+  '/',
+  authMiddleware,
+  userTypeMiddleware(['TEACHER', 'STUDENT']),
+  questionController.getAllQuestions
+);
+router.get(
+  '/:id',
+  authMiddleware,
+  userTypeMiddleware(['TEACHER', 'STUDENT']),
+  questionController.getQuestionById
+);
+router.delete(
+  '/:id',
+  authMiddleware,
+  userTypeMiddleware(['TEACHER']),
+  questionController.deleteQuestionById
+);
+router.put(
+  '/:id',
+  authMiddleware,
+  userTypeMiddleware(['TEACHER']),
+  questionController.updateQuestionById
+);
 
 module.exports = router;
